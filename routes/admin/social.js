f0827807cb2e8314d975a5713d60dc88760722ee
Ryan Conrad <ryan.conrad@imanage.com>
2018-08-22 14:45:21 -0500

    add db and tables

--- routes/admin/social.js
+++ routes/admin/social.js
@@ -11,11 +11,35 @@ let database = new Database("social");
 
 router.get("/", (req, res, next) => {
 	try {
-		return res.render("social/index", { layout: "material" });
+		let resdata = {};
+		database.open()
+			.then(() => {
+				return database.tables.networks.all();
+			})
+			.then((data) => {
+				resdata.networks = data;
+				return database.tables.accounts.all();
+				
+			})
+			.then((data) => {
+				resdata.accounts = data;
+				return res.render("social/index", {
+					data: resdata,
+					layout: "material",
+					page: { title: "SOCIAL" }
+				});
+			})
+			.then(() => {
+				console.log("close database");
+				return database.close();
+			})
+			.catch((err) => {
+				console.error(err);
+				return next(new Error(err));
+			});
 	} catch (e) {
 		return next(e);
 	}
 });
-
 
 module.exports = router;
