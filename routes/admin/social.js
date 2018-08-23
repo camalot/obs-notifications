6d4b73246d6c0cc21e640acb7cc595592a035eb7
Ryan Conrad <ryan.conrad@imanage.com>
2018-08-23 17:13:05 -0500

    add/update

--- routes/admin/social.js
+++ routes/admin/social.js
@@ -1,12 +1,11 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
 // const multer = require('multer');
 // const upload = multer({ dest: 'public/images/social/' });
-const config = require('./social.config');
-const utils = require('../../lib/utils');
-const merge = require('merge');
-
+const config = require("./social.config");
+const utils = require("../../lib/utils");
+const merge = require("merge");
 
 const Database = require("../../lib/data/database");
 let database = new Database("social");
@@ -14,16 +13,16 @@ let database = new Database("social");
 router.get("/", (req, res, next) => {
 	try {
 		let resdata = {};
-		database.open()
+		database
+			.open()
 			.then(() => {
 				return database.tables.networks.all();
 			})
-			.then((data) => {
+			.then(data => {
 				resdata.networks = data;
 				return database.tables.accounts.all();
-				
 			})
-			.then((data) => {
+			.then(data => {
 				resdata.accounts = data;
 				return res.render("social/index", {
 					data: resdata,
@@ -35,7 +34,7 @@ router.get("/", (req, res, next) => {
 				console.log("close database");
 				return database.close();
 			})
-			.catch((err) => {
+			.catch(err => {
 				console.error(err);
 				return next(new Error(err));
 			});
@@ -44,30 +43,85 @@ router.get("/", (req, res, next) => {
 	}
 });
 
-
 router.post("/network", (req, res, next) => {
 	try {
 		console.log(req.body);
-		database.open()
+		database
+			.open()
 			.then(() => {
 				return database.tables.networks.add({
-					name: req.body['name'],
-					color: req.body['fgcolor'],
-					background: req.body['bgcolor'],
-					background2: req.body['bgcolor2'],
-					image: req.body['image']
+					name: req.body["name"],
+					color: req.body["fgcolor"],
+					background: req.body["bgcolor"],
+					background2: req.body["bgcolor2"],
+					image: req.body["image"]
 				});
 			})
-			.then((data) => {
+			.then(data => {
 				return res.redirect("/admin/social");
 			})
 			.then(() => {
 				return database.close();
 			})
-			.catch((err) => {
+			.catch(err => {
+				return next(err);
+			});
+	} catch (e) {
+		return next(e);
+	}
+});
+
+router.get("/account/:id/enabled/:enabled", (req, res, next) => {
+	try {
+		let enabled = req.params.enabled === "true" || req.params.enabled === "1" ? true : false;
+		console.log("enabled: " + enabled);
+		return database
+			.open()
+			.then(() => {
+				return database.tables.accounts.update({
+					id: req.params.id,
+					enabled: enabled
+				});
+			})
+			.then((data) => {
+				return res.json(data);
+			})
+			.then(() => {
+				return database.close();
+			})
+			.catch(err => {
 				return next(err);
 			});
 
+	} catch (e) {
+		return next(e);
+	}
+});
+
+router.post("/account", (req, res, next) => {
+	console.log(req.body);
+	let netid = parseInt(req.body.network || "0");
+	let enabled = parseInt(req.body.enabled || "0") === 1;
+
+	try {
+		database
+			.open()
+			.then(() => {
+				return database.tables.accounts.add({
+					network_id: netid,
+					name: req.body.name,
+					enabled: enabled
+				});
+			})
+			.then(data => {
+				return res.redirect("/admin/social");
+			})
+			.then(() => {
+				return database.close();
+			})
+			.catch(err => {
+				return next(err);
+			});
 	} catch (e) {
 		return next(e);
 	}
