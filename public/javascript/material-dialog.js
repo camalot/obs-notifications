842c900e5da40dc201ccf15a3e6887a5d04f9a2f
Ryan Conrad <camalot@gmail.com>
2018-12-27 09:47:40 -0600

    add polyfill for dialog

--- public/javascript/material-dialog.js
+++ public/javascript/material-dialog.js
@@ -3,10 +3,12 @@
 $(function () {
 	$("[data-dialog]").each(function (i) {
 		let dialogId = $(this).data("dialog");
-		console.log(`initialize ${dialogId}`);
 		$(this).click(function () {
 			let dialog = $(`#${dialogId}`).get(0);
 			if (dialog) {
+				if (dialogPolyfill) {
+					dialogPolyfill.registerDialog(dialog);
+				}
 				dialog.showModal();
 			}
 		});
@@ -17,8 +19,6 @@ $(function () {
 		let dialog = $(this).closest("dialog").get(0);
 		if (dialog) {
 			dialog.close();
-		} else {
-			console.error("this shit doesn't work");
 		}
 	});
 });
