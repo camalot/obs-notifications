0e079f5eeaa3f50d5fcd97bff849a0b0ee7bcde2
Ryan Conrad <camalot@gmail.com>
2019-05-30 14:53:58 -0500

    settings smoothie

--- public/javascript/heartmonitor/ecg.js
+++ public/javascript/heartmonitor/ecg.js
@@ -1,7 +1,13 @@
 	(function ($) {
 		$(function () {
-
+			var low = 30;
 			var smoothie = new SmoothieChart({
+				responsive: true,
+				minValue: low - 5,
+				maxValue: 100,
+				scaleSmoothing: 0.125,
+				maxValueScale: 1,
+				minValueScale: 1,
 				grid: {
 					strokeStyle: 'rgb(0, 0, 0,0)', fillStyle: 'rgb(0, 0, 0,0)',
 					lineWidth: 1, millisPerLine: 250, verticalSections: 6,
@@ -17,9 +23,11 @@
 			var socket = new WebSocket("ws://192.168.2.3:3880");
 			socket.onopen = function (e) {
 				console.log('Connected to server!');
+				ecgData.append(new Date().getTime(), low);
 			};
 			socket.onclose = function (e) {
 				console.log('Disconnected from server');
+				ecgData.append(new Date().getTime(), low);
 			};
 			socket.onmessage = function (e) {
 				var data = JSON.parse(e.data);
@@ -27,6 +35,8 @@
 					console.log(data.bpm);
 					if (ecgData) {
 						ecgData.append(new Date().getTime(), data.bpm);
+						ecgData.append(new Date().getTime() + 50, low);
+
 					}
 				}
 			};
