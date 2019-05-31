9409c66f2ddb5f30e76e63da14ffe46ade505d0d
Ryan Conrad <ryan.conrad@imanage.com>
2019-05-31 15:21:11 -0500

    reconnect test

--- public/javascript/heartmonitor/bpm.js
+++ public/javascript/heartmonitor/bpm.js
@@ -1,22 +1,40 @@
 (function ($) {
 	$(function () {
-		var socket = new WebSocket("ws://192.168.2.3:3880");
-		socket.onopen = function (e) {
-			console.log('Connected to server!');
-			$(".hms-value").empty();
-		};
-		socket.onclose = function (e) {
-			console.log('Disconnected from server');
-			$(".hms-value").empty();
-		};
-		socket.onmessage = function (e) {
-			var data = JSON.parse(e.data);
-			if (data.bpm) {
-				$(".hms-value").html(data.bpm);
-			}
-		};
-		socket.onerror = function (e) {
-			console.log('An error has occurred!\n' + e.message);
-		};
+		const SOCKET_ENDPOINT = "ws://192.168.2.3:3880";
+		let reconnectPointer = null;
+		let socket;
+
+		function _startWS(socketAddress) {
+
+			socket = new WebSocket();
+			socket.onopen = function (e) {
+				console.log('Connected to server!');
+				$(".hms-value").empty();
+			};
+			socket.onclose = function (e) {
+				$(".hms-value").empty();
+				console.log('Disconnected from server');
+				clearTimeout(reconnectPointer);
+				console.log("Reconnect attempt in 5 seconds.");
+				socket = null;
+				reconnectPointer = setTimeout(() => {
+					console.log("Attempting to reconnect...");
+					start(socketAddress);
+				}, 5000);
+			};
+			socket.onmessage = function (e) {
+				let data = JSON.parse(e.data);
+				if (data.bpm) {
+					$(".hms-value").html(data.bpm);
+				} else {
+					$(".hms-value").empty();
+				}
+			};
+			socket.onerror = function (e) {
+				console.error('An error has occurred!\n' + e.message);
+			};
+		}
+
+		_startWS(SOCKET_ENDPOINT);
 	});
 })(jQuery);
--- public/javascript/heartmonitor/ecg.js
+++ public/javascript/heartmonitor/ecg.js
@@ -1,12 +1,50 @@
-	(function ($) {
-		$(function () {
-			var diff = Date.now() - new Date("04/01/1977").getTime();
-			var ageDate = new Date(diff);
-			var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+(function ($) {
+	$(function () {
+		const SOCKET_ENDPOINT = "ws://192.168.2.3:3880";
+		let reconnectPointer = null;
+		let socket;
+		function _startWS(socketAddress) {
 
+			socket = new WebSocket();
+			socket.onopen = function (e) {
+				console.log('Connected to server!');
+				ecgData.append(new Date().getTime(), low);
+			};
+			socket.onclose = function (e) {
+				console.log('Disconnected from server');
+				ecgData.append(new Date().getTime(), low);
 
-			var low = 20;
-			var smoothie = new SmoothieChart({
+				clearTimeout(reconnectPointer);
+				console.log("Reconnect attempt in 5 seconds.");
+				socket = null;
+				reconnectPointer = setTimeout(() => {
+					console.log("Attempting to reconnect...");
+					start(socketAddress);
+				}, 5000);
+			};
+			socket.onmessage = function (e) {
+				let data = JSON.parse(e.data);
+				if (data.bpm) {
+					console.log(data.bpm);
+					if (ecgData) {
+						ecgData.append(new Date().getTime() - 200, low);
+						ecgData.append(new Date().getTime(), data.bpm);
+						ecgData.append(new Date().getTime() + 200, low);
+					}
+				}
+			};
+			socket.onerror = function (e) {
+				console.error('An error has occurred!\n' + e.message);
+			};
+		}
+
+		function _initSmoothie() {
+
+			let diff = Date.now() - new Date("04/01/1977").getTime();
+			let ageDate = new Date(diff);
+			let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+			let low = 20;
+			let smoothie = new SmoothieChart({
 				responsive: true,
 				minValue: 0,
 				//maxValue: 220 - (age + 5),
@@ -15,38 +53,27 @@
 				maxValueScale: 1,
 				minValueScale: 1,
 				grid: {
-					strokeStyle: 'rgb(0, 0, 0, 0)', fillStyle: 'rgb(0, 0, 0, 0)',
-					lineWidth: 1, millisPerLine: 1000, verticalSections: 0,
+					strokeStyle: 'rgb(0, 0, 0, 0)',
+					fillStyle: 'rgb(0, 0, 0, 0)',
+					lineWidth: 1,
+					millisPerLine: 1000,
+					verticalSections: 0,
 				},
-				labels: { fillStyle: 'rgb(0, 0, 0, 0)' }
+				labels: {
+					fillStyle: 'rgb(0, 0, 0, 0)'
+				}
 			});
 			smoothie.streamTo(document.getElementById("ecg-smoothie"));
-			var ecgData = new TimeSeries();
-			smoothie.addTimeSeries(ecgData, { strokeStyle: 'rgb(255, 0, 0, 0.5)', fillStyle: 'rgba(255, 0, 0, 0)', lineWidth: 15});
+			let ecgData = new TimeSeries();
+			smoothie.addTimeSeries(ecgData, {
+				strokeStyle: 'rgb(255, 0, 0, 0.5)',
+				fillStyle: 'rgba(255, 0, 0, 0)',
+				lineWidth: 15
+			});
+		}
 
-			var socket = new WebSocket("ws://192.168.2.3:3880");
-			socket.onopen = function (e) {
-				console.log('Connected to server!');
-				ecgData.append(new Date().getTime(), low);
-			};
-			socket.onclose = function (e) {
-				console.log('Disconnected from server');
-				ecgData.append(new Date().getTime(), low);
-			};
-			socket.onmessage = function (e) {
-				var data = JSON.parse(e.data);
-				if (data.bpm) {
-					console.log(data.bpm);
-					if (ecgData) {
-						ecgData.append(new Date().getTime() - 200, low);
-						ecgData.append(new Date().getTime(), data.bpm);
-						ecgData.append(new Date().getTime() + 200, low);
+		_initSmoothie();
+		_startWS(SOCKET_ENDPOINT);
 
-					}
-				}
-			};
-			socket.onerror = function (e) {
-				console.log('An error has occurred!\n' + e.message);
-			};
-		});
-	})(jQuery);
+	});
+})(jQuery);
